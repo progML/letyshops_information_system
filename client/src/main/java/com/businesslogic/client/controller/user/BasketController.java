35afcc21ee3979f7cea09a060b1202f30fe25145
matveylesonen@gmail.com <Vesnushka99>
2021-09-20 15:41:16 +0300

    Embedding a variable

--- client/src/main/java/com/businesslogic/client/controller/user/BasketController.java
+++ client/src/main/java/com/businesslogic/client/controller/user/BasketController.java
@@ -20,17 +20,14 @@ public class BasketController {
 
     private final ClientService clientService;
 
-    private int userId;
 
 
     //добавление в корзину
     @PostMapping("/basket/add")
     public ResponseEntity<String> addOrder(@RequestBody BasketEntityDto basketEntityDto, Principal principal) {
-
         if (basketService.checkDeviceId(basketEntityDto.getDevice_id()) > 0) {
             try {
-                userId = clientService.findByLogin(principal.getName()).getId();
-                basketService.addOrder(basketEntityDto, userId);
+                basketService.addOrder(basketEntityDto, clientService.findByLogin(principal.getName()).getId());
                 return new ResponseEntity<>("Товары добавлены в корзину", HttpStatus.OK);
             } catch (Exception e) {
                 return new ResponseEntity<>("Ваша корзина пуста", HttpStatus.NOT_FOUND);
@@ -44,9 +41,8 @@ public class BasketController {
     //удаление товара из корзины
     @DeleteMapping("/basket/{id}")
     public ResponseEntity<String> deleteItem(@PathVariable Integer id, Principal principal) {
-        userId = clientService.findByLogin(principal.getName()).getId();
         try {
-            if (basketService.findUserByBasketId(id) == userId) {
+            if (basketService.findUserByBasketId(id) == clientService.findByLogin(principal.getName()).getId()) {
                 basketService.deleteBasket(id);
                 return new ResponseEntity<>("Удаление из корзины прошло успешно", HttpStatus.OK);
             } else {
@@ -62,7 +58,7 @@ public class BasketController {
     //  просмотр корзины
     @GetMapping("/basket")
     public ResponseEntity<Object> findAll(Principal principal) {
-        userId = clientService.findByLogin(principal.getName()).getId();
+        int userId = clientService.findByLogin(principal.getName()).getId();
         if (basketService.findAllById(userId).isEmpty()) {
             return new ResponseEntity<>("Корзина пуста", HttpStatus.OK);
         } else {
@@ -74,9 +70,8 @@ public class BasketController {
     //итогавая стоимость в корзине
     @GetMapping("basket/sum")
     public ResponseEntity getPrice(Principal principal) {
-        userId = clientService.findByLogin(principal.getName()).getId();
         return new ResponseEntity(
-                "Итогавая стоимоть: " + basketService.findSum(userId) + " Кэшбэк составит: " + basketService.findCashback(),
+                "Итогавая стоимоть: " + basketService.findSum(clientService.findByLogin(principal.getName()).getId()) + " Кэшбэк составит: " + basketService.findCashback(),
                 HttpStatus.OK);
     }
 
